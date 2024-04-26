34f80dd02f28de67f58987d130c45d2a771f2ed1
Kevin Opoku-Agyemang <kevino.agyemang@gmail.com>
2024-04-26 18:59:41 -0400

    misc change

--- aggregatorservice/__main__.py
+++ aggregatorservice/__main__.py
@@ -21,5 +21,5 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(
-        "newsaggregatorservice.__main__:app", host="0.0.0.0", port=5007, reload=True
+        "aggregatorservice.__main__:app", host="0.0.0.0", port=5007, reload=True
     )
